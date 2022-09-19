7cbbb35116f27f40d941ac1c469278776536d7af
Shane Clark <ff.shane.clark@outlook.com>
2022-09-19 00:47:53 -0600

    Complete redesign.

--- assets/js/frostfire.js
+++
@@ -1,5 +0,0 @@
-jQuery(window).ready(function() {
-    particlesJS.load("particles-js", "assets/json/particles.json", function () {
-        console.log("callback - particles config loaded")
-    });
-});
